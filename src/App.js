74f63d4eb90510e9bf11ea48026a48c9a86b2c32
nejahneji <nejinejah6@gmail.com>
2021-06-16 13:14:55 +0100

    jsx1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import img2 from "./img2.jpg";
+import "./App.css"
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div style={{border:"solid 1px black",maxWidth:"100vw"}}>
+        <h1 className="title red">Nejah</h1>
+
+        <br />
+
+        <img src="/photo/img1.jpg" alt="" />
+
+        <br />
+
+        <img src={img2} alt="" />
+      </div>
+
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/L9ZYdShgtPE"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
     </div>
   );
-}
+};
 
 export default App;
